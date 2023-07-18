bfd1604f7a73ecb325277643e8012a993be9f123
SeongYong <pji50260@naver.com>
2023-07-18 23:12:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,22 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Optional, List
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class TMList(BaseModel):
     start_station: str
