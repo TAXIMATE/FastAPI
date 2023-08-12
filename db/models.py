304e70a852d0f0c442e9dcec27da25f183cf7b23
SeongYong <pji50260@naver.com>
2023-08-12 17:12:25 +0900

    Update models.py

--- db/models.py
+++ db/models.py
@@ -1,51 +1,55 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean
 from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.sqltypes import DateTime
-from sqlalchemy.ext.declarative import declarative_base 
 
-Base = declarative_base()  
+Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
-
     id = Column(Integer, primary_key=True, index=True)
     nickname = Column(String, unique=True, index=True)
     profile = Column(String, unique=True, index=True)
     thumbnail = Column(String)
 
-class TM(Base):
-    __tablename__ = "tms"
+class Station(Base):
+    __tablename__ = "stations"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
 
+class Team(Base):
+    __tablename__ = "teams"
     id = Column(Integer, primary_key=True, index=True)
     start_station = Column(String, index=True)
-    team_leader = Column(Integer, ForeignKey("users.id"))
-    member_1 = Column(Integer, ForeignKey("users.id"))
-    member_2 = Column(Integer, ForeignKey("users.id"))
-    member_3 = Column(Integer, ForeignKey("users.id"))
     end_station = Column(String, index=True)
     desired_departure = Column(DateTime)
     comment = Column(String)
     in_progress = Column(Boolean)
 
-    @property
-    def current_members(self):
-        members = [self.team_leader, self.member_1, self.member_2, self.member_3]
-        return len([member for member in members if member is not None])
-
-
 class Comment(Base):
     __tablename__ = "comments"
-
     id = Column(Integer, primary_key=True, index=True)
-    tm_id = Column(Integer, ForeignKey("tms.id"))
+    team_id = Column(Integer, ForeignKey("teams.id"))
     timestamp = Column(DateTime)
     written_by = Column(Integer, ForeignKey("users.id"))
 
-class Temperature(Base):
-    __tablename__ = "temperatures"
-
+class Rating(Base):
+    __tablename__ = "ratings"
     id = Column(Integer, primary_key=True, index=True)
     user_id = Column(Integer, ForeignKey("users.id"))
-    tm_id = Column(Integer, ForeignKey("tms.id"))
-    temperature = Column(Integer)  
+    rating = Column(Integer)
 
+class Temperature(Base):
+    __tablename__ = "temperatures"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    team_id = Column(Integer, ForeignKey("teams.id"))
+    temperature = Column(Integer)
+
+class UserAuth(Base):
+    __tablename__ = "user_auth"
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id"))
+    auth_provider = Column(String)
+    auth_id = Column(String)
+    auth_token = Column(String)
