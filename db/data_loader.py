ede459207bff0407f68dbacc708bff76f800c9c9
SeongYongPark <pji50260@naver.com>
2023-08-14 16:54:38 +0900

    08_14

--- db/data_loader.py
+++ db/data_loader.py
@@ -4,13 +4,16 @@ from datetime import datetime
 from sqlalchemy.orm import Session
 from db_session import engine, get_new_session
 from models import Station, Team
+import os
 
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+stations_csv_path = os.path.join(current_dir, 'stations.csv')
 
 def insert_stations(db: Session):
-    with open('stations.csv', 'r', encoding='utf-8') as file:
-        reader = csv.reader(file)
-        for row in reader:
-            station_name = row[0]
+    with open(stations_csv_path, 'r', encoding='utf-8') as file:
+        for line in file:
+            station_name = line.strip()
             station = Station(name=station_name)
             db.add(station)
         db.commit()
@@ -19,7 +22,7 @@ def insert_teams(db: Session):
     for _ in range(30):
         team = Team(
             start_station_id=random.randint(1, 100),  # 실제 station id 범위에 맞게 변경
-            end_station="임의의 역",
+            end_station="서울역",
             desired_departure=datetime.now(),  # 현재 시간 또는 원하는 시간으로 설정
             comment="이것은 코멘트입니다.",
             in_progress=random.choice([True, False])
